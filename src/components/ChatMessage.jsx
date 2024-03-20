fe2d18ca07e3160f7518da2a673437b7105c1534
barteek-poz <szowior4@wp.pl>
2024-03-20 10:09:48 +0100

    fixed messages styles

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,9 +1,9 @@
 import ROBOT_ICON from "../assets/robot.svg";
 const ChatMessage = ({answer}) => {
   return (
-    <div className="MESSAGE_BOX flex items-start justify-start gap-1 ml-4 mr-2 my-4">
+    <div className="MESSAGE_BOX flex items-start justify-start 2xl:justify-center gap-1 ml-4 mr-2 my-4">
       <img src={ROBOT_ICON} alt="user-icon" className="USER_ICON w-7 h-7" />
-      <div className="MESSAGE_BG bg-cream max-w-3/4  p-3 rounded-[20px]">
+      <div className="MESSAGE_BG bg-cream max-w-[700px]  p-3 rounded-[20px]">
         <p className="MESSAGE_TEXT text-mainBlue font-light">
           {answer}
         </p>
--- src/components/UserMessage.jsx
+++ src/components/UserMessage.jsx
@@ -1,14 +1,14 @@
 import USER_ICON from "../assets/user.svg";
 const UserMessage = ({question}) => {
   return (
-    <div className="MESSAGE_BOX flex items-start justify-end gap-1 mr-4 ml-2 my-4">
+    <div className="MESSAGE_BOX flex items-start justify-end 2xl:justify-center gap-1 mr-4 ml-2 my-4">
       
-      <div className="MESSAGE_BG bg-mainBlue max-w-3/4  p-3 rounded-[20px]">
+      <div className="MESSAGE_BG bg-mainBlue max-w-3/4  p-3 rounded-[20px] 2xl:order-2">
         <p className="MESSAGE_TEXT text-white font-light">
           {question}
         </p>
       </div>
-      <img src={USER_ICON} alt="user-icon" className="USER_ICON w-7 h-7" />
+      <img src={USER_ICON} alt="user-icon" className="USER_ICON w-7 h-7 2xl:order-1" />
     </div>
   );
 };
